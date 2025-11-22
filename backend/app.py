6380b2fe50ffa17a31c04c492c9f8c65d5fb2376
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-11-22 19:37:36 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .database import Base, engine
-from .routers import auth_router
+from database import Base, engine
+from routers import auth_router
 
 app = FastAPI(title="Datainsight SST Suite")
 
@@ -15,3 +15,4 @@ app.include_router(auth_router.router)
 @app.get("/health")
 def health():
     return {"status": "OK"}
+
