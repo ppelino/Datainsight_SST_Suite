fe0a8eedc9f9dc45da34c92b6444c06e33f51b83
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-11-22 20:08:15 -0300

    Update auth_router.py

--- backend/routers/auth_router.py
+++ backend/routers/auth_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from import models, schemas
+import models, schemas
 from auth import hash_password, verify_password, create_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -34,4 +34,5 @@ def login(data: schemas.UserLogin, db: Session = Depends(get_db)):
 
     token = create_token({"id": user.id, "email": user.email})
     return {"access_token": token}
+
 
