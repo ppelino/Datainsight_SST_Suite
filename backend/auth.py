e875235f7223a6ddb83b4ac12fb8e06fc83fde9b
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2025-11-22 21:35:48 -0300

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -2,18 +2,24 @@ from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
 
-SECRET_KEY = "CHAVE_SUPER_SECRETA"  # depois troca
+SECRET_KEY = "CHAVE_SUPER_SECRETA"  # depois você troca por uma env var
 ALGORITHM = "HS256"
 
-pwd = CryptContext(schemes=["bcrypt"])
+# Troquei bcrypt -> pbkdf2_sha256
+pwd = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
-def hash_password(password: str):
+
+def hash_password(password: str) -> str:
     return pwd.hash(password)
 
-def verify_password(password: str, hashed: str):
+
+def verify_password(password: str, hashed: str) -> bool:
     return pwd.verify(password, hashed)
 
-def create_token(data: dict):
+
+def create_token(data: dict) -> str:
     payload = data.copy()
     payload["exp"] = datetime.utcnow() + timedelta(hours=24)
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
